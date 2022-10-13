1ced577d498831465b05d31a29d955cde912311a
Chhay-Lay <95404380+Chhay-Lay@users.noreply.github.com>
2022-10-13 08:43:14 +0700

    Add files via upload
    check IPv4 if correct or not

---
+++ IPV4_test.php
@@ -0,0 +1,29 @@
+<?php
+	function checkIPv4($ip): int
+	{
+		$each_part = explode('.', $ip);
+		
+		if(substr_count($ip, '.') === 3) 
+		{
+			foreach($each_part as $part)
+			{
+				if($part[0] === 0)
+				{
+					echo ('invalid IPv4');
+					return false;
+				}
+				else if((intval($part) > 255) || (intval($part) < 0))
+				{
+					echo ('invalid IPv4');
+					return false;
+				}
+			}
+			echo('valid IPv4');
+			return true;
+		}else{
+			echo ('invalid IPv4');
+			return false;
+		}
+	}
+	
+	checkIPv4('192.168.1.1');
